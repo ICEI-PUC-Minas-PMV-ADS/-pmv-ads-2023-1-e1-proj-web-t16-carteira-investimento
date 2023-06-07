7eb4ff16ef01672c4d3287fe1e26f6338149761a
Marco Binda <43681402+marcobinda@users.noreply.github.com>
2023-06-07 14:35:03 -0300

    Update por_equity.js
    Implementada totalização de patrimônio e totalização por ativo

--- src/js/por_equity.js
+++ src/js/por_equity.js
@@ -7,55 +7,68 @@ function getData() {
 
 var ativoSet = new Set();
 function getAssets() {
-  getData();
   for (let i = 0; i < tradeData.length; i++) {
     ativoSet.add(tradeData[i].data.ativo);
   }
 }
 
-function getEquity() {
+function totalValueEquity() {
+
+  getData();
+
+  let valorTotal = 0.0;
+
+  for (let i = 0; i < tradeData.length; i++) {
+
+    if (tradeData[i].data.operacao == "Compra") {
+      valorTotal += parseFloat(tradeData[i].data.valorTotal);
+    } 
+    else if (tradeData[i].data.operacao == "Venda") {
+      valorTotal -= parseFloat(tradeData[i].data.valorTotal);
+    } 
+    else {
+      continue;
+    }
+  }; 
+  console.log(valorTotal);
+}
+
+function totalValueByEquity() {
+
   getData();
   getAssets();
 
-  for (let ativo of ativoSet) {
-    // console.log(ativo);
+  let patrimonio = new Array();
 
-    // let patrimonio = new Array();
+  for (ativo of ativoSet) {
+
+    let tempData = new Array();
     let quantidade = 0;
     let valorTotal = 0.0;
 
     for (let i = 0; i < tradeData.length; i++) {
-      // console.log(tradeData[i].data);
-      // console.log("i ", i);
-      // console.log("ativoSet ", ativo);
-      // console.log("ativoTradeData ", tradeData[i].data.ativo);
-      console.log("operacao ", tradeData[i].data.operacao);
-      // console.log("ativo = ativo", ativo === tradeData[i].data.ativo);
-      console.log("operacao Compra", tradeData[i].data.operaco == 0);
-      console.log("operacao Venda", tradeData[i].data.operaco == 1);
 
-      if (ativo == tradeData[i].data.ativo & tradeData[i].data.operaco == 0) {
-        valorTotal += parseDouble(tradeData[i].data.valorTotal);
+      if (ativo == tradeData[i].data.ativo && tradeData[i].data.operacao == "Compra") {
+        valorTotal += parseFloat(tradeData[i].data.valorTotal);
         quantidade += parseInt(tradeData[i].data.quantidade);
       } 
-      else if (ativo == tradeData[i].data.ativo & tradeData[i].data.operaco == 1) {
-        valorTotal -= parseDouble(tradeData[i].data.valorTotal);
+      else if (ativo == tradeData[i].data.ativo && tradeData[i].data.operacao == "Venda") {
+        valorTotal -= parseFloat(tradeData[i].data.valorTotal);
         quantidade -= parseInt(tradeData[i].data.quantidade);
       } 
+      else {
+        continue;
+      }
 
-
-      // console.log(ativo);
-      console.log("quantidade ", quantidade);
-      console.log("valorTotal ", valorTotal);
-
-      // patrimonio[i] = {
-      //   "ativo": ativo,
-      //   "quantidade": quantidade,
-      //   "valorTotal": valorTotal
-      // }
+      tempData = {
+        ativo,
+        quantidade,
+        valorTotal      
+      }; 
     }
+    patrimonio.push(tempData);
   }
-  // console.log(patrimonio);
+  console.log(patrimonio);
 }
 
 
