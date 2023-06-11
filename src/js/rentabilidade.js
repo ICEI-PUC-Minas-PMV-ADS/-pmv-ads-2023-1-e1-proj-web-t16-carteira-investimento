661cb993880c35a237f4daf413b8d813b6c1b7fb
Marco Binda <43681402+marcobinda@users.noreply.github.com>
2023-06-11 14:37:05 -0300

    20230611 rentabilidade
    Correção do cálculo da rentabilidade se `aporte` estiver vazio (estava dando erro, retornando NaN por ser "" e ser transformada para Float).

--- src/js/rentabilidade.js
+++ src/js/rentabilidade.js
@@ -1,17 +1,38 @@
 function calculate() {
-  var principal = parseFloat(document.getElementById("principal").value);
-  var aporte = parseFloat(document.getElementById("aporte").value);
-  var time = parseInt(document.getElementById("time").value);
-  var interest = parseFloat(document.getElementById("interest").value)/100;
+
+  var principal = document.getElementById("principal").value;
+  var aporte = document.getElementById("aporte").value;
+  var time = document.getElementById("time").value;
+  var interest = document.getElementById("interest").value;
+
+  console.log("principal",principal);
+  console.log("aporte",aporte,typeof(aporte));
+  console.log("time",time);
+  console.log("interest",interest);
+
   // var juros = interest / 12;
 
+  var principal = parseFloat(principal);
+  var aporte = aporte !== "" ? parseFloat(aporte) : 0;
+  var time = parseInt(time);
+  var interest = parseFloat(interest)/100;
 
-  if (aporte == null || aporte == 0 || aporte == "") {
+  console.log("principal2",principal);
+  console.log("aporte2",aporte,typeof(aporte));
+  console.log("time2",time);
+  console.log("interest2",interest);
+
+  if (aporte == null || aporte == 0 || aporte =="") {
+    console.log(aporte == null);
+    console.log(aporte == "");
+    console.log(aporte == 0);
     var total = principal * Math.pow(1 + interest, time);
   } else {
-    var totalAporte = aporte * (Math.pow(1 + interest, time) - 1) / interest;
-    var total = principal * Math.pow(1 + interest, time) + totalAporte;
+    var totalAporte = (aporte * (Math.pow(1 + interest, time) - 1)) / interest;
+    var total = (principal * Math.pow(1 + interest, time)) + totalAporte;
   }
+
+  console.log(total.toFixed(2).replace('.', ','));
 
 
   document.getElementById("result").innerHTML = "Valor Final: R$" + total.toFixed(2).replace('.', ',');
